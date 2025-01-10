c8ffb1d0422f37453b6ef6550a7ebeaa77c605d6
Prince Clumson-Eklu <14794665+pclumson@users.noreply.github.com>
2025-01-09 23:39:33 -0800

    Update tag.dart
    Add the fromJson method to Tag

--- expense_manager/lib/models/tag.dart
+++ expense_manager/lib/models/tag.dart
@@ -7,4 +7,13 @@ class Tag {
     required this.id,
     required this.name,
   });
+
+ 
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+  
 }
